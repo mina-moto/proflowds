3c82817668c8e41eb4f2743c2c1982475472f59b
mina-moto <minamoto.it@gmail.com>
2020-06-26 03:35:31 +0900

    Update analyze.py
    refactaring, change docstring.

--- proflowds/analyze/analyze.py
+++ proflowds/analyze/analyze.py
@@ -1,6 +1,6 @@
-'''
+"""
 Module for data analyze.
-'''
+"""
 
 import pandas as pd
 import seaborn as sns
@@ -8,27 +8,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import math
+from typing import Any
 
 
-def output_csv(df: pd.DataFrame, output_data_path):
+def output_csv(df: pd.DataFrame, output_data_path: str):
     """
-    Output df to csv(output_data_path)
+    Output df as a csv file to output_data_path.
     Args:
-        df:
-        output_data_path:
+        df:Data
+        output_data_path:Output path of the csv file
     """
     os.makedirs(os.path.dirname(output_data_path), exist_ok=True)
     df.to_csv(output_data_path)
 
 
-def make_missing_table(df) -> pd.DataFrame:
+def make_missing_table(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Make missing_table of df
+    Make missing values table of df.
     Args:
-        df:
-
+        df:Data
     Returns:
-
+        Number and percentage of missing values for each column.
     """
     null_val = df.isnull().sum()
     missing_rate = df.isnull().sum() / len(df)
@@ -39,70 +39,40 @@ def make_missing_table(df) -> pd.DataFrame:
     return missing_table
 
 
-def move_column_end(df, column):
-    '''
-    Move column to last
-    :param df:
-    :param column:
-    :return:
-    '''
-    columns = df.columns.tolist()
-    columns.remove(column)
-    columns.append(column)
-    df = df[columns]
-    return df
-
-
-def transform_yeo_johnson():
-    return
-
-
-def plot_relation_target(df, target_column, output_data_path="relation.png"):
-    '''
-    Plot relation to the target_column
-    '''
-    # relation to the target
-    col_n = df.shape[1]
-    y_line = math.ceil(col_n / 5)
-    fig = plt.figure(figsize=(16, 3 * y_line))
-    for i in np.arange(col_n):
-        ax = fig.add_subplot(y_line, 5, i + 1)
-        sns.regplot(x=df.iloc[:, i], y=df[target_column])
-    plt.tight_layout()
-    # plt.show()
-    plt.savefig(output_data_path)
-
-
-def top_correlation_column(df, target_column, num=None):
+def top_correlation_column(
+        df: pd.DataFrame,
+        target: str,
+        num: int = None) -> Any:
     """
-    :param df:pandas data frame
-        data
-    :param target_column:str
-    :param num:int
-        If not set, it is the number of columns of df.corr().
+    Extract the columns with high correlation to the target in df.
+    Args:
+        df:Data
+        target:Column of target
+        num:Number of columns to be extracted
+            If not set, it is the number of columns of df.corr().
+    Returns:
+        Column Index, following format
+        Index(['column1', 'column2'], dtype='object')
     """
     corrmat = df.corr()
     if num is None:
         num = len(corrmat)
-    cols = corrmat.abs().nlargest(num, target_column)[target_column].index
+    cols = corrmat.abs().nlargest(num, target)[target].index
     return cols
 
 
-def plot_corr_heatmap(df, target_column, num=None):
+def plot_corr_heatmap(df, target, num=None):
     """
-    Visualize heatmap top num target_columns abs value of correlation with collumn
-    :param df:pandas data frame
-        data
-    :param target_column:str
-    :param num:int
-        If not set, it is the number of columns of df.corr().
+    Show heatmap the columns with high correlation to the target in df.
+     Args:
+        df:Data
+        target:Column of target
+        num:Number of columns to be extracted
+            If not set, it is the number of columns of df.corr().
     """
-
-    cols = top_correlation_column(df, target_column, num)
-
+    cols = top_correlation_column(df, target, num)
     if num is None:
         num = len(cols)
-
     cm = df[cols].corr()
     sns.set(font_scale=1.5)
     fig, ax = plt.subplots(figsize=(num, num))
@@ -119,37 +89,3 @@ def plot_corr_heatmap(df, target_column, num=None):
         xticklabels=cols.values)
     ax.set_ylim(num, 0)
     plt.show()
-
-
-def top_num_index(num, l):
-    '''
-    Top num index list of l
-    '''
-    li = np.array(l).argsort()[::-1]
-    tni = li[:num]
-    return tni
-
-
-def make_feature_importances_table(x_train, model, num=10):
-    '''
-    Top num columns feature importance using learned model.
-    :return pd.DataFrame
-    '''
-    feature_importances_index = top_num_index(num, model.feature_importances_)
-    feature_importances_table = pd.DataFrame(
-        {"column": x_train.columns.values[feature_importances_index],
-         "importances": model.feature_importances_[feature_importances_index]})
-    return feature_importances_table
-
-
-def plot_feature_importances(x_train, model, num=10):
-    '''
-    Plot top num columns feature importance using learned model.
-    '''
-    feature_importances_table = make_feature_importances_table(
-        x_train, model, num=num)
-    fig, ax = plt.subplots(figsize=(11, 11))
-    ax.set_xlabel("feature importance")
-    plt.tight_layout()
-    sns.barplot(x=feature_importances_table["importances"],
-                y=feature_importances_table["column"], orient='h')
